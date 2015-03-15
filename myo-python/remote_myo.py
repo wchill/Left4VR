8a15668f55e8ae1a115e3fda5a7144c997a53617
Eric Ahn <wchill1337@gmail.com>
2015-03-15 01:11:19 -0500

    Myo support

---
+++ myo-python/remote_myo.py
@@ -0,0 +1,133 @@
+# Copyright (C) 2014  Niklas Rosenstein
+# All rights reserved.
+
+import myo
+from myo.lowlevel import pose_t, stream_emg
+from myo.six import print_
+import random
+
+import socket
+import os, sys
+sys.path.append(os.path.join('Users', 'wchill', 'Projects', 'Left4VR', 'myo-python'))
+
+myo.init()
+sock = socket.create_connection(('192.168.1.156', 8000))
+sock.setblocking(0)
+
+SHOW_OUTPUT_CHANCE = 0.00
+MELEEMINSPEED = 150
+MELEETRUE = False
+last_gyro = None
+r"""
+There can be a lot of output from certain data like acceleration and orientation.
+This parameter controls the percent of times that data is shown.
+"""
+
+class Listener(myo.DeviceListener):
+    # return False from any method to stop the Hub
+
+    def on_connect(self, myo, timestamp):
+        print_("Connected to Myo")
+        myo.vibrate('short')
+        myo.request_rssi()
+
+    def on_rssi(self, myo, timestamp, rssi):
+        print_("RSSI:", rssi)
+
+    def on_event(self, event):
+        r""" Called before any of the event callbacks. """
+
+    def on_event_finished(self, event):
+        r""" Called after the respective event callbacks have been
+        invoked. This method is *always* triggered, even if one of
+        the callbacks requested the stop of the Hub. """
+
+    def on_pair(self, myo, timestamp):
+        print_('Paired')
+        print_("If you don't see any responses to your movements, try re-running the program or making sure the Myo works with Myo Connect (from Thalmic Labs).")
+        print_("Double tap enables EMG.")
+        print_("Spreading fingers disables EMG.\n")
+
+    def on_disconnect(self, myo, timestamp):
+        print_('on_disconnect')
+
+    def on_pose(self, myo, timestamp, pose):
+        print_('on_pose', pose)
+        if pose == pose_t.double_tap:
+            print_("Enabling EMG")
+            print_("Spreading fingers disables EMG.")
+            print_("=" * 80)
+            myo.set_stream_emg(stream_emg.enabled)
+        elif pose == pose_t.fingers_spread:
+            print_("=" * 80)
+            print_("Disabling EMG")
+            myo.set_stream_emg(stream_emg.disabled)
+
+    def on_orientation_data(self, myo, timestamp, orientation):
+        show_output('orientation', orientation)
+
+    def on_accelerometor_data(self, myo, timestamp, acceleration):
+        show_output('acceleration', acceleration)
+
+    def on_gyroscope_data(self, myo, timestamp, gyroscope):
+        global last_gyro
+        if last_gyro is None or last_gyro[2] - gyroscope[2] > 50 or last_gyro[2] - gyroscope[2] < -50:
+            last_gyro = gyroscope
+            print gyroscope
+#        show_output('gyroscope', gyroscope)
+        if gyroscope[2]  > MELEEMINSPEED or gyroscope[2] < -MELEEMINSPEED:
+            melee()
+        else:
+            meleeReset()
+
+    def on_unlock(self, myo, timestamp):
+        print_('unlocked')
+
+    def on_lock(self, myo, timestamp):
+        print_('locked')
+
+    def on_sync(self, myo, timestamp, arm, x_direction):
+        print_('synced', arm, x_direction)
+
+    def on_unsync(self, myo, timestamp):
+        print_('unsynced')
+        
+    def on_emg(self, myo, timestamp, emg):
+        show_output('emg', emg)
+
+
+def show_output(message, data):
+    if random.random() < SHOW_OUTPUT_CHANCE: 
+        print_(message + ':' + str(data))
+
+def melee():
+    global MELEETRUE
+    if not MELEETRUE:
+        print 'melee'
+        sock.send('M1\r\n')
+        MELEETRUE = True
+
+def meleeReset():
+    global MELEETRUE
+    if MELEETRUE:
+        print 'no melee'
+        MELEETRUE = False
+        sock.send('M0\r\n')
+
+def main():
+    hub = myo.Hub()
+    hub.set_locking_policy(myo.locking_policy.none)
+    hub.run(1000, Listener())
+
+    # Listen to keyboard interrupts and stop the
+    # hub in that case.
+    try:
+        while hub.running:
+            myo.time.sleep(0.2)
+    except KeyboardInterrupt:
+        print_("Quitting ...")
+        hub.stop(True)
+
+if __name__ == '__main__':
+    main()
+
